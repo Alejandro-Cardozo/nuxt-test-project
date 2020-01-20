5c82c4bcf189964c2f604cfa474d5a003f2a1ad4
Alejandro Cardozo <uri.cardozo@gmail.com>
2020-01-20 16:07:09 -0300

    init + setup

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>about us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione sit asperiores quidem facere hic ea quis dolore totam voluptates animi at aliquam doloribus, porro qui tenetur eligendi atque nesciunt. Sequi.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "First Nuxt App | About",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "this is a simple website about posts"
+        }
+      ],
+      htmlAttrs: {
+        lang: "en"
+      }
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Welcome to my first Nuxt App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "First Nuxt App | Home",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "this is a simple website about posts"
+        }
+      ],
+      htmlAttrs: {
+        lang: "en"
+      }
+    };
+  }
+};
+</script>
